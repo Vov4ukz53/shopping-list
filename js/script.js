767a2dbdd0c3a285c182505b385d32f9979c3148
vov4ukz53 <vov4ukz53@gmail.com>
2021-10-15 21:03:52 +0200

    adding a new product to the list

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 {
-   const products = [
-      {
-         content: "czekolada",
-      },
-   ];
+   const products = [];
 
    const render = () => {
       let htmlString = "";
 
       for (const product of products) {
-            htmlString += `
+         htmlString += `
             <li class="shoppingList__item">
             <button class="shoppingList__added"></button>
-            ${products.content}
+            ${product.content}
             <button class="shoppingList__delete"></button>
             </li>
 			`;
@@ -21,10 +17,33 @@
       document.querySelector(".js-products").innerHTML = htmlString;
    };
 
-   const init = () => {
+   const addNewProduct = (newProduct) => {
+
+      products.push({
+         content: newProduct,
+      });
 
       render();
+   };
 
+
+
+   const init = () => {
+      render();
+
+      const form = document.querySelector(".js-form");
+
+      form.addEventListener("submit", (event) => {
+         event.preventDefault();
+
+         const newProduct = document.querySelector(".js-newProduct").value.trim();
+
+         if (newProduct === "") {
+            return
+         }
+
+         addNewProduct(newProduct);
+      });
    };
 
    init();
