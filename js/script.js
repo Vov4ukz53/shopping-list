d80e01f3cf9843e09c099c9a0de88b2982484f20
vov4ukz53 <vov4ukz53@gmail.com>
2021-10-15 20:17:30 +0200

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,62 +1,31 @@
 {
-	const calculateResult = (amount, eur, usd, gbp, currency) => {
+   const products = [
+      {
+         content: "czekolada",
+      },
+   ];
 
-		switch (currency) {
-			case "eur":
-				return amount * eur;
+   const render = () => {
+      let htmlString = "";
 
-			case "usd":
-				return amount * usd;
+      for (const product of products) {
+            htmlString += `
+            <li class="shoppingList__item">
+            <button class="shoppingList__added"></button>
+            ${products.content}
+            <button class="shoppingList__delete"></button>
+            </li>
+			`;
+      }
 
-			case "gbp":
-				return amount * gbp;
-		}
-	};
+      document.querySelector(".js-products").innerHTML = htmlString;
+   };
 
-	const changeResultText = (amount, result, currency) => {
-		const calculatorResult = document.querySelector(".js-calculator-result");
-		calculatorResult.innerText = `${amount.toFixed(2)} pln = ${result.toFixed(2)} ${currency}`;
-	};
+   const init = () => {
 
-	const changeInfoText = (eur, usd, gbp, currency) => {
-		const calculatorChange = document.querySelector(".js-calculator-change");
+      render();
 
-		switch (currency) {
-			case "eur":
-				return calculatorChange.innerText = `${eur} ${currency.toUpperCase()}`;
+   };
 
-			case "usd":
-				return calculatorChange.innerText = `${usd} ${currency.toUpperCase()}`;
-
-			case "gbp":
-				return calculatorChange.innerText = `${gbp} ${currency.toUpperCase()}`;
-		}
-	};
-
-	const onFormSubmit = (event) => {
-		event.preventDefault();
-
-		const eur = 0.22;
-		const usd = 0.25;
-		const gbp = 0.19;
-
-		const formInput = document.querySelector(".js-form-input");
-		const formSelect = document.querySelector(".js-form-select");
-
-		const amount = +formInput.value;
-		const currency = formSelect.value;
-
-		const result = calculateResult(amount, eur, usd, gbp, currency);
-
-		changeResultText(amount, result, currency);
-		changeInfoText(eur, usd, gbp, currency);
-	};
-
-	const init = () => {
-		const form = document.querySelector(".js-form");
-
-		form.addEventListener("submit", onFormSubmit);
-	};
-
-	init();
+   init();
 }
