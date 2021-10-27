bd0a83fa7908f453be7cb0bf16c42e947f4e5aad
vov4ukz53 <vov4ukz53@gmail.com>
2021-10-27 18:49:12 +0200

    error correction

--- js/script.js
+++ js/script.js
@@ -45,11 +45,16 @@
 
 		for (const product of products) {
 			htmlString += `
-            <li class="shoppingList__item${product.done && hideDoneProducts === true ? " shoppingList__item--hidden" : ""}">
+            <li class="shoppingList__item${product.done && hideDoneProducts
+					? " shoppingList__item--hidden"
+					: ""}">
             <button class="shoppingList__itemButton js-buttonMark${product.done
-					? " shoppingList__itemButton--done" : ""}">
+					? " shoppingList__itemButton--done"
+					: ""}">
 				</button>
-				<span class="shoppingList__itemContent${product.done ? " shoppingList__itemContent--done" : ""}"">
+				<span class="shoppingList__itemContent${product.done
+					? " shoppingList__itemContent--done"
+					: ""}"">
 				${product.content}
 				</span>
             <button class="shoppingList__itemButton shoppingList__itemButton--delete js-remove"></button>
@@ -63,19 +68,19 @@
 	const renderButtons = () => {
 		const buttonsProducts = document.querySelector(".js-buttons");
 
-		if (products.length === 0) {
+		if (!products.length) {
 			return
 		}
 
 		buttonsProducts.innerHTML = `
 			<button class="section__button js-hideDoneProducts">
-				${products.some(({ done }) => done) && hideDoneProducts === true
-				? "Pokaż ukończone"
-				: "Ukryj ukończone"}
+				${products.some(({ done }) => done) && hideDoneProducts
+				? "Pokaż zaznaczone"
+				: "Ukryj zaznaczone"}
 			</button>
 			<button${products.every(({ done }) => done) ? " disabled" : ""}
 			class="section__button js-doneAllProducts">
-				Ukoncz wszystkie
+				Zaznacz wszystkie
 			</button$>
 		`;
 	};
@@ -113,7 +118,7 @@
 		const hideDoneProductsElement = document.querySelector(".js-hideDoneProducts");
 		const doneAllProductsElement = document.querySelector(".js-doneAllProducts");
 
-		if (doneAllProductsElement !== null && hideDoneProductsElement !== null) {
+		if (doneAllProductsElement && hideDoneProductsElement) {
 			doneAllProductsElement.addEventListener("click", () => {
 				doneAllProducts();
 			});
